9d236a18569adffd7f8841a2358a494e80a6583a
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2020-11-24 22:41:30 +0000

    Added OverlayImage method to overlay images on top of other images

--- ImageObject.cs
+++ ImageObject.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace ChristmasWallpaper
 {
@@ -14,6 +15,30 @@ namespace ChristmasWallpaper
         public ImageObject(String imageFilePath)
         {
             image = Image.FromFile(imageFilePath);
+        }
+
+        public Image GetImage()
+        {
+            return image;
+        }
+
+        public void OverlayImage(ImageObject overlay)  // Overlay the given image on top of this one
+        {
+            Image overlayImageData = overlay.GetImage();
+
+            // Calculate coordinates such that overlay image is vertically at bottom of base image and horizontally centred
+            int xValue = (image.Width / 2) - (overlayImageData.Width / 2);
+            int yValue = image.Height - overlayImageData.Height;
+            
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                g.DrawImage(overlayImageData, new Point(xValue, yValue));
+            }
+        }
+
+        public void SaveImage(String path)
+        {
+            image.Save(path, image.RawFormat);
         }
     }
 }
