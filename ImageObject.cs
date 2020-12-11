a5ee5d3106626588b57b2619b1d68941af1d81e5
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2020-12-11 21:53:07 +0000

    Modified OverlaySmallerImage to compensate for taskbar.

--- ImageObject.cs
+++ ImageObject.cs
@@ -29,17 +29,45 @@ namespace ChristmasWallpaper
             // Make sure overlay image is smaller than base image- resize if necessary
             int overlayWidth = overlayImageData.Width;
             int overlayHeight = overlayImageData.Height;
-            
+            int taskbarOffsetX = 0;
+            int taskbarOffsetY = 0;
+
+            string taskbarPos = WindowsWallpaper.GetTaskbarPosition();
+            if (!WindowsWallpaper.TaskbarIsHidden()) {
+                if (taskbarPos == "Left" || taskbarPos == "Right")
+                {
+                    // Taskbar uses space on x axis
+                    taskbarOffsetX = WindowsWallpaper.GetTaskbarHeight();
+                }
+                else
+                {
+                    // Taskbar uses space on y axis
+                    taskbarOffsetY = WindowsWallpaper.GetTaskbarHeight();
+                }
+            }
             // If overlay is bigger in either dimension then shrink by 10% until it is smaller
-            while (image.Width < overlayWidth || image.Height < overlayHeight)
+            while (image.Width - taskbarOffsetX < overlayWidth || image.Height - taskbarOffsetY < overlayHeight)
             {
                 overlayWidth = (int)(overlayWidth * 0.9);
                 overlayHeight = (int)(overlayHeight * 0.9);
             }
+            
+            
             // Calculate coordinates such that overlay image is vertically at bottom of base image and horizontally centred
             int xValue = (image.Width / 2) - (overlayWidth / 2);
-            int yValue = image.Height - overlayHeight;
-            
+            int yValue = (image.Height - overlayHeight);
+
+            if (taskbarPos == "Right" || taskbarPos == "Bottom")
+            {
+                // Need to subtract offset
+                taskbarOffsetX *= -1;
+                taskbarOffsetY *= -1;
+            }
+
+            xValue += taskbarOffsetX;
+            yValue += taskbarOffsetY;
+
+
             using (Graphics g = Graphics.FromImage(image))
             {
                 // Must use rectange overload instead of point or overlay image may be resized
